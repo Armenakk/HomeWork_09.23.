59d87aa9ca2cb5f860cd1445029a22deba24205a
«Armenak <armenakk@yandex.ru>
2023-09-22 19:28:21 +0300

    commit Task_008

---
+++ Task_008/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите любое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+bool not = true;
+while (count <= number)
+{
+    if (count % 2 != 1)
+    {
+        Console.Write(count + ", ");
+        not = false;
+    }
+    count++;
+}
+if (not)
+{
+    Console.WriteLine("К сожалению четных чисел нет!");
+}
