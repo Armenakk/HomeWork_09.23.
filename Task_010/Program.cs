cefa845ebbe5483fea4fb1f3cacdaf06bae9c906
«Armenak <armenakk@yandex.ru>
2023-09-28 08:40:53 +0300

    Выполнил задания к ДЗ 2

---
+++ Task_010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введите любое целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int Secondnum = number / 10;
+    int Secondnumber = Secondnum % 10;
+    Console.WriteLine($"Последняя цифра {Secondnumber}");
+}
+else
+{
+    Console.WriteLine($"такая цифра не подходит");
+}
---
+++ Task_013/Program.cs
@@ -0,0 +1,23 @@
+﻿// // // Напишите программу, которая выводит третью цифру 
+// // // заданного числа или сообщает, что третьей цифры нет.
+// // // 645 -> 5
+// // // 78 -> третьей цифры нет
+// // // 32679 -> 6
+
+
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+int num = number;
+string result = "В числе " + number + " третьей цифры нет.";
+
+if (number > 100)
+{
+    while (num > 1000)
+    {
+        num /= 10;
+    }
+    num = num % 10;
+    result = "Третья цифра в числе " + number + " -> " + num;
+}
+Console.WriteLine(result);
