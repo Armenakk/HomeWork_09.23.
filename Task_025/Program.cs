7b5a8fe801cb9a395c3332b6241bff6f45ae9443
«Armenak <armenakk@yandex.ru>
2023-10-05 09:21:10 +0300

    commit Task_25

---
+++ Task_025/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который 
+// 1. принимает на вход два числа (A и B) и 
+// 2. возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int riseToPower = RiseToPower(num, power);
+Console.WriteLine($"Число {num} в степени {power} будет равно {riseToPower}");
+
+int RiseToPower(int number, int count)
+{
+    int result = 1;
+    for (int i = 1; i <= count; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
