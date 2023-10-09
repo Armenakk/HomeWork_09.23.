27fcc668bf2acfb2e5c5fb3f4252e7d95619bb86
«Armenak <armenakk@yandex.ru>
2023-10-09 09:30:53 +0300

    commit Task_041

---
+++ Task_041/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: 
+// 1. Пользователь вводит с клавиатуры M чисел. 
+// 2. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+Console.WriteLine("В этом массиве: ");
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(-100, 100);
+    }
+}
+
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+if (numbers[i] > 0)
+count++;
+}
+Console.WriteLine($"из {numbers.Length} чисел {count} -> больше 0");
+
+void PrintArray(int[] numbers )
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + ", ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
